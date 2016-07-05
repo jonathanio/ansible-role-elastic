49c634d7b4d2efcf89436f826c3950c6e6619525
Jonathan Wright <jon@than.io>
2016-07-05 22:20:22 +0100

    Update tests based on installation defaults

--- test/integration/elasticsearch/serverspec/elasticsearch_spec.rb
+++ test/integration/elasticsearch/serverspec/elasticsearch_spec.rb
@@ -7,38 +7,55 @@ case os[:family]
 when 'debian','ubuntu'
   package_name = 'elasticsearch'
   service_name = 'elasticsearch'
+  config_dir = '/etc/elasticsearch'
   config_file = '/etc/elasticsearch/elasticsearch.yml'
   logging_file = '/etc/elasticsearch/elasticsearch.yml'
+  config_owner = 'root'
+  config_group = 'elasticsearch'
 when 'redhat','centos'
   package_name = 'elasticsearch'
   service_name = 'elasticsearch'
+  config_dir = '/etc/elasticsearch'
   config_file = '/etc/elasticsearch/elasticsearch.yml'
   logging_file = '/etc/elasticsearch/elasticsearch.yml'
+  config_owner = 'root'
+  config_group = 'elasticsearch'
 else
   package_name = 'elasticsearch'
   service_name = 'elasticsearch'
+  config_dir = '/etc/elasticsearch'
   config_file = '/etc/elasticsearch/elasticsearch.yml'
   logging_file = '/etc/elasticsearch/elasticsearch.yml'
+  config_owner = 'root'
+  config_group = 'elasticsearch'
 end
 
 describe package(package_name) do
   it { should be_installed }
 end
 
+describe file(config_dir) do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 750 }
+  it { should be_owned_by config_owner }
+  it { should be_grouped_into config_group }
+end
+
 describe file(config_file) do
   it { should exist }
   it { should be_file }
   it { should be_mode 644 }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
+  it { should be_owned_by config_owner }
+  it { should be_grouped_into config_group }
 end
 
 describe file(logging_file) do
   it { should exist }
   it { should be_file }
   it { should be_mode 644 }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
+  it { should be_owned_by config_owner }
+  it { should be_grouped_into config_group }
 end
 
 describe service(service_name) do
